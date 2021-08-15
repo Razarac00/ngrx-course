79bd2916f4774554d869240cad62386e4716748b
John Orji <john.orji@cognizant.com>
2021-08-15 18:13:34 -0500

    Added feature selector for type-safe access

--- src/app/auth/auth.selectors.ts
+++ src/app/auth/auth.selectors.ts
@@ -1,11 +1,17 @@
-import { createSelector } from "@ngrx/store";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { AuthState } from "./reducers";
+
+export const selectAuthState = createFeatureSelector<AuthState>(
+    "auth" // the feature module from the store that we want to select
+);
 
 // a mapping function that acts as a store query specifically to grab the auth from the appstate and perform the 
 // !! check on it. We use the selector to optimize this: it stores it inMemory so we don't have to keep generating
 // it, avoid re-assigning the variable with a value when the value hasn't changed, and performing a call to access 
 // that part of the state. This is a 'memoized' function
 export const isLoggedIn = createSelector(
-    state => state["auth"], // input state object
+    // state => state["auth"], // input state object
+    selectAuthState, // type safe version of the above indexing
     (auth) => !!auth.user // projector function
 );
 

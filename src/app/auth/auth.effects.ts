535434c87ca3747feb4acd8c2bf36b0c88dd138e
John Orji <john.orji@cognizant.com>
2021-08-15 19:18:30 -0500

    side effect with local storage implemented--end of section 3

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { tap } from "rxjs/operators";
 import { AuthActions } from "./action-types";
 
+// make sure to stop server when creating effects
 @Injectable()
 export class AuthEffects {
 
@@ -16,7 +18,20 @@ export class AuthEffects {
         // note: without this being set to false, the login action would have kept going infinitely
     ); // createEffect auto subscribes and does error-handling to protect against corruption of the observable
 
-    constructor(private actions$: Actions) {
+
+    logout$ = createEffect(() => 
+            this.actions$
+            .pipe(
+                ofType(AuthActions.logout),
+                tap(action => {
+                    localStorage.removeItem('user');
+                    this.router.navigateByUrl('/login');
+                })
+            )
+            , {dispatch: false}
+    );
+
+    constructor(private actions$: Actions, private router: Router) {
         // a side effect is something extra we do in the app AFTER an action has been dispatched--post reducer
 
         // actions$.subscribe(action => {

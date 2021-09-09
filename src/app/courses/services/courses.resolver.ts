b1b18334fb259deae79b819e097ccc23a7b65e94
John Orji <john.orji@cognizant.com>
2021-09-09 13:55:11 -0500

    Setup resolver to load courses only once from the backend

--- src/app/courses/services/courses.resolver.ts
+++ src/app/courses/services/courses.resolver.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { filter, first, map, tap } from "rxjs/operators";
 import { CourseEntityService } from "./course-entity.service";
 
 @Injectable()
@@ -12,11 +12,23 @@ export class CoursesResolver implements Resolve<boolean> {
     }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        // using the loaded$ observable, we get our boolean needed to resolve the route
+        // using tap, we create the 'side-effect' of actually putting the course data in the store
+        // we then filter on true loaded values only, and return on the first of those true values
+        return this.coursesService.loaded$.pipe(
+            tap(loaded => {
+                if (!loaded) {
+                    this.coursesService.getAll();
+                }
+            }),
+            filter(loaded => !!loaded),
+            first()
+        );
         // ngrx will perform the get by making assumptions: pluralize the entity 'course' with an 's', the path will be presumed
         // to be 'api/courses/, and the response from said path is expected to be the actual course[]
         // depending on the backend service's implementation, this can automatically work, but in our case it doesn't
-        return this.coursesService.getAll().pipe(
-            map(courses => !!courses) // we return true iff there are courses, AND the get will store the course[] in the store
-        );
+        // this.coursesService.getAll().pipe(
+        //     map(courses => !!courses) // we return true iff there are courses, AND the get will store the course[] in the store
+        // );
     }
 }
